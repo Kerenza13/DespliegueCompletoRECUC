9f44970d2d21e276179fe98e4d487f205fadbbf4
Kerenza13 <carlichesjunior@gmail.com>
2025-05-27 19:10:56 +0200

    Refactor frontend dependencies and update React version; enhance App component for better loading message; modify CSS for font family; adjust Vite configuration for plugin usage. Remove obsolete Nginx configuration files. Implement RECUCController for database interaction and create Message entity for ORM. Add HTML template for DbController response. Initialize package-lock.json and set up Dockerfile for web service with Nginx configuration.

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
-CREATE TABLE secretosRECUC (
- id INT AUTO_INCREMENT PRIMARY KEY,
- fraseRECUC VARCHAR(255) NOT NULL
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255) NOT NULL
 );
-INSERT INTO secretosRECUC (fraseRECUC) VALUES ('La base de datos responde, buenos días Carlos Morillas Delgado!');
+INSERT INTO messages (content) VALUES ('La base de datos responde, Holaaa!');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
 function App() {
- const [message, setMessage] = useState('');
- useEffect(() => {
- fetch('/api/db')
- .then((res) => res.json())
- .then((data) => setMessage(data.message));
- }, []);  
- return (
- <div>
- <h1>Frontend en React de CarlosMorillasDelgado</h1>
- <p>Esta aplicación se conecta al backend de Symfony pidiéndole una respuesta</p>
- <p>respuesta del Backend: {message || 'Loading...'}</p>
- </div>
- );
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/db')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend en React de Carlos Morillas Delgado</h1>
+      <p>Esta aplicación se conecta al backend de Symfony pidiéndole una respuesta</p>
+      <p>respuesta del Backend: {message || 'Espera un poco que el backend no está listo...'}</p>
+    </div>
+  );
 }
+
 export default App;
