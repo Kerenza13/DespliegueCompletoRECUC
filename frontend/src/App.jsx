11bb9bf26762d8c01d305c881c0b53fa24b5e9f0
Kerenza13 <carlichesjunior@gmail.com>
2025-05-28 21:38:18 +0200

    fix: update route in RECUCController to '/api/RECUC'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api/db')
+    fetch('/api/RECUC')
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
